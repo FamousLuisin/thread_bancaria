baa6f0dcf6b7bd6b6790b5a501beed2c87ab9c51
FamousLuisin <67298012+FamousLuisin@users.noreply.github.com>
2024-04-26 17:23:33 -0300

    Criando a função Comprar no Banco

--- thread_bancaria/src/thread_bancaria/Banco.java
+++ thread_bancaria/src/thread_bancaria/Banco.java
@@ -1,5 +1,29 @@
 package thread_bancaria;
 
-public class Banco {
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class Banco {
+	private Lock lock = new ReentrantLock();
+	private List<Loja> contasCorporativas;
+	
+	public void comprar(Cliente cliente, double valor) {
+		lock.lock();
+		
+		try {
+			Random rand = new Random();
+			int indice = rand.nextInt(contasCorporativas.size());
+			
+			Loja loja = contasCorporativas.get(indice);
+			
+			System.out.println("Transferencia de " + valor + "R$");
+			cliente.getConta().debitar(valor);
+			loja.getConta().creditar(valor);
+			
+		} finally {
+			lock.unlock();
+		}
+	}
 }
